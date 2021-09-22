6265ad898a992f7d20f9746e4a60e8648bab8242
Rogerio Shieh Barbosa <40327446+rogerioshieh@users.noreply.github.com>
2021-09-22 10:28:42 -0400

    [semver:major] Add suggestions from partnership review (#4)
    * Add suggestions from partnership review
    
    * Change main description
    
    * Make description shorter
    
    * rename example and add parameter to command
    
    * fix api-token type
    
    * add default for api-token param
    
    * Inject param into bash script via env var
    
    * Remove unnecessary exports
    
    * add check for errors
    
    * Increase wait time in API request loop
    
    Co-authored-by: elliott-king <8884571+elliott-king@users.noreply.github.com>

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 
 [![CircleCI Build Status](https://circleci.com/gh/shipyardbuild/circleci-orb.svg?style=shield "CircleCI Build Status")](https://circleci.com/gh/shipyardbuild/circleci-orb) [![CircleCI Orb Version](https://badges.circleci.com/orbs/shipyard/shipyard.svg)](https://circleci.com/orbs/registry/orb/shipyard/shipyard) [![GitHub License](https://img.shields.io/badge/license-MIT-lightgrey.svg)](https://raw.githubusercontent.com/shipyardbuild/circleci-orb/master/LICENSE) [![CircleCI Community](https://img.shields.io/badge/community-CircleCI%20Discuss-343434.svg)](https://discuss.circleci.com/c/ecosystem/orbs)
 
-This orb can be used to connect with Shipyard during a CircleCI job and fetch relevent environment variables.
+Use Shipyard with CircleCI to run CircleCI jobs on Staging Environments automatically deployed by Shipyard, authenticating into them via a bypass token. 
+
+This orb connects with Shipyard during a CircleCI job, fetching necessary environment variables in order to run e2e tests where authentication via OAuth is normally required.
+
 
 ## Resources
 
--- src/scripts/get-shipyard-environment.sh
+++ src/scripts/get-shipyard-environment.sh
@@ -6,6 +6,10 @@ ORG="${CIRCLE_PROJECT_USERNAME}"
 REPO="${CIRCLE_PROJECT_REPONAME}"
 BRANCH="${CIRCLE_BRANCH}"
 
+# Get auth token
+SHIPYARD_API_TOKEN=${!PARAM_SHIPYARD_TOKEN}
+[ -z "$SHIPYARD_API_TOKEN" ] && echo "A Shipyard API token must be supplied. Check the \"api-token\" parameter." && exit 1
+
 # Hit the Shipyard API
 URL="https://shipyard.build/api/v1/project?org_name=${ORG}&repo_name=${REPO}&branch=${BRANCH}"
 JSON=$(curl -s "${URL}" -H "x-api-token: ${SHIPYARD_API_TOKEN}")
@@ -14,6 +18,14 @@ JSON=$(curl -s "${URL}" -H "x-api-token: ${SHIPYARD_API_TOKEN}")
 if [ -z "${JSON}" ]
 then
   echo "ERROR: no response from ${URL}"
+  exit 1
+fi
+
+# Ensure there are no other errors
+if [ "$(echo "${JSON}" | jq -r '.errors[0]')" != null ]
+then
+  ERROR=$(echo "${JSON}" | jq -r '.errors[0].title')
+  echo "ERROR: ${ERROR}."
   exit 1
 fi
 
@@ -43,7 +55,7 @@ fi
 # Wait for the environment to be ready
 while [[ "${SHIPYARD_ENVIRONMENT_READY}" != true ]]; do
   echo "Waiting for your Shipyard environment..."
-  sleep 3
+  sleep 15
 
   # Re-fetch the data
   JSON=$(curl -s "${URL}" -H "x-api-token: ${SHIPYARD_API_TOKEN}")
