cf19c7fd998244348ae18a4533f06fa9a822cc86
Peter Valdez <peter@shipyard.build>
2021-12-13 21:18:01 -0500

    Install pip if needed

--- src/scripts/orb.sh
+++ src/scripts/orb.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env sh
 
-# Install Python if needed
+# Install Python
 if ! which python > /dev/null; then
     echo "Trying to install Python..."
-    which apt-get > /dev/null && apt-get update -qq && apt-get install -qq python
-    which yum > /dev/null && yum install -y python
+    which apt-get > /dev/null && apt-get update -qq && apt-get install -qq python && echo Installed!
+    which yum > /dev/null && yum install -y python && echo Installed!
 fi
 
-# Install wget if needed
+# Install pip
+if ! which pip > /dev/null; then
+    echo "Trying to install pip..."
+    which apt-get > /dev/null && apt-get update -qq && apt-get install -qq python-pip && echo Installed!
+    which yum > /dev/null && yum install -y python-pip && echo Installed!
+fi
+
+# Install wget
 if ! which wget > /dev/null; then
     echo "Trying to install wget..."
-    which apt-get > /dev/null && apt-get update -qq && apt-get install -qq wget
-    which yum > /dev/null && yum install -y wget
+    which apt-get > /dev/null && apt-get update -qq && apt-get install -qq wget && echo Installed!
+    which yum > /dev/null && yum install -y wget && echo Installed!
 fi
 
 # Download the orb
