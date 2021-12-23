d85ae055299d70bf59acca219154a096b13c5a18
Peter Valdez <peter@shipyard.build>
2021-12-23 15:29:43 -0500

    Get rid of all unnecessary output

--- src/scripts/orb.sh
+++ src/scripts/orb.sh
@@ -3,30 +3,51 @@
 # Install Python
 if ! which python > /dev/null; then
     echo "Trying to install Python..."
-    which apt-get > /dev/null && apt-get update -qq && apt-get install -qq python && echo Installed!
-    which yum > /dev/null && yum install -y python && echo Installed!
+
+    which apt-get > /dev/null && \
+        apt-get update -qq > /dev/null && \
+        apt-get install -qq python > /dev/null && \
+        echo Installed!
+
+    which yum > /dev/null && \
+        yum install -y python > /dev/null && \
+        echo Installed!
 fi
 
 # Install pip
 if ! which pip > /dev/null; then
     echo "Trying to install pip..."
-    which apt-get > /dev/null && apt-get update -qq && apt-get install -qq python-pip && echo Installed!
-    which yum > /dev/null && yum install -y python-pip && echo Installed!
+
+    which apt-get > /dev/null && \
+        apt-get update -qq > /dev/null && \
+        apt-get install -qq python-pip > /dev/null && \
+        echo Installed!
+
+    which yum > /dev/null && \
+        yum install -y python-pip > /dev/null && \
+        echo Installed!
 fi
 
 # Install wget
 if ! which wget > /dev/null; then
     echo "Trying to install wget..."
-    which apt-get > /dev/null && apt-get update -qq && apt-get install -qq wget && echo Installed!
-    which yum > /dev/null && yum install -y wget && echo Installed!
+
+    which apt-get > /dev/null && \
+        apt-get update -qq > /dev/null && \
+        apt-get install -qq wget > /dev/null && \
+        echo Installed!
+
+    which yum > /dev/null && \
+        yum install -y wget > /dev/null && \
+        echo Installed!
 fi
 
 # Download the orb
 cd /tmp || exit
-wget https://github.com/shipyardbuild/circleci-orb/archive/refs/heads/convert-to-python.tar.gz
-tar xvzf convert-to-python.tar.gz
+wget https://github.com/shipyardbuild/circleci-orb/archive/refs/heads/convert-to-python.tar.gz > /dev/null
+tar xvzf convert-to-python.tar.gz > /dev/null
 cd /tmp/circleci-orb-convert-to-python/src/scripts || exit
 
 # Run the orb
-pip install -r requirements.txt
+pip install -r requirements.txt > /dev/null
 python orb.py 
