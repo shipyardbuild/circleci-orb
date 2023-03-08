ca00b175776313e6af6fad53d418c01f13781fda
Akshay K <akshay@shipyard.build>
2023-03-08 19:28:26 +0530

    chore: update python (#18)
    * chore: update python
    
    * chore: add python orb
    
    * fix: lint error
    
    * chore: install pip
    
    * fix: add app-dir
    
    * revert: python orb
    
    * test: if python already exists
    
    * test: print python version
    
    * test: check python3 presence
    
    * chore: install python3 if missing
    
    * fix: expired certs
    
    * fix: use sudo if available
    
    * test: add more prints about versions
    
    * fix: manually ln python & python3
    
    * fix: use force when creating sys link
    
    * revert: extra print statements
    
    * fix: update certs
    
    * fix: pass force flag while creating sys links for pip
    
    * chore: comment out local orb
    
    ---------
    
    Co-authored-by: Akshay Kalia <akshay@pillar.tech>

--- src/scripts/orb.sh
+++ src/scripts/orb.sh
@@ -5,18 +5,23 @@ if [ "$(id -u)" = 0 ]; then export SUDO=""; else # Check if we are root
   export SUDO="sudo";
 fi
 
+# Fix Cert error - https://www.omgubuntu.co.uk/2017/08/fix-google-gpg-key-linux-repository-error
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | $SUDO apt-key add -
+
 # Install Python
-if ! which python > /dev/null; then
+if ! which python3 --version > /dev/null; then
     echo "Trying to install Python..."
 
     which apt-get > /dev/null && \
         $SUDO apt-get update -qq > /dev/null && \
-        $SUDO apt-get install -qq python > /dev/null && \
+        $SUDO apt-get install -qq python3 python3-six > /dev/null && \
         echo Installed!
 
     which yum > /dev/null && \
-        yum install -y python > /dev/null && \
+        yum install -y python3 python3-six > /dev/null && \
         echo Installed!
+
+    $SUDO ln -sf /usr/bin/python3 /usr/bin/python > /dev/null
 fi
 
 # Install pip
@@ -25,12 +30,14 @@ if ! which pip > /dev/null; then
 
     which apt-get > /dev/null && \
         $SUDO apt-get update -qq > /dev/null && \
-        $SUDO apt-get install -qq python-pip > /dev/null && \
+        $SUDO apt-get install -qq python3-pip > /dev/null && \
         echo Installed!
 
     which yum > /dev/null && \
-        yum install -y python-pip > /dev/null && \
+        yum install -y python3-pip > /dev/null && \
         echo Installed!
+
+    $SUDO ln -sf /usr/bin/pip3 /usr/bin/pip > /dev/null
 fi
 
 # Install wget
