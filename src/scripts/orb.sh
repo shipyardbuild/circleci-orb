502d4c6dc0cffab6ab5e15406fa3a24dbe6d9fb3
Akshay Kalia <akshay@shipyard.build>
2024-03-13 15:06:00 -0400

    update install script

--- src/scripts/orb.sh
+++ src/scripts/orb.sh
@@ -56,10 +56,10 @@ fi
 
 # Download the orb
 cd /tmp || exit
-wget -q https://github.com/shipyardbuild/circleci-orb/archive/refs/heads/master.tar.gz
-tar xvzf master.tar.gz > /dev/null
+wget -q https://github.com/shipyardbuild/circleci-orb/archive/refs/heads/chore-pick-correct-hash.tar.gz
+tar xvzf chore-pick-correct-hash.tar.gz > /dev/null
 
-cd /tmp/circleci-orb-master/src/scripts || exit
+cd /tmp/circleci-orb-chore-pick-correct-hash/src/scripts || exit
 
 # Run the orb
 pip install -r requirements.txt > /dev/null
