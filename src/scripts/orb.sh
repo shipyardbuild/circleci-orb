d02feeb4e03ddf4a2459d6218c4643740e661c16
Akshay K <akshay@shipyard.build>
2024-12-17 22:35:09 +0530

    chore: update gpg (#23)
    * update gpg
    
    * try virtual envs
    
    * fix postix error
    
    * try pipx
    
    * try python-env
    
    * add ignore comment
    
    * remove pipx
    
    * code cleanup
    
    * chore: revert test code
    
    * fix output

--- src/scripts/orb.sh
+++ src/scripts/orb.sh
@@ -6,7 +6,8 @@ if [ "$(id -u)" = 0 ]; then export SUDO=""; else # Check if we are root
 fi
 
 # Fix Cert error - https://www.omgubuntu.co.uk/2017/08/fix-google-gpg-key-linux-repository-error
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | $SUDO apt-key add -
+wget -q -O /usr/share/keyrings/google-keyring.gpg https://dl.google.com/linux/linux_signing_key.pub
+echo "deb [signed-by=/usr/share/keyrings/google-keyring.gpg] https://dl.google.com/linux/chrome/deb/ stable main" | $SUDO tee /etc/apt/sources.list.d/google-chrome.list > /dev/null
 
 # Install Python
 if ! which python3 --version > /dev/null; then
@@ -14,7 +15,7 @@ if ! which python3 --version > /dev/null; then
 
     which apt-get > /dev/null && \
         $SUDO apt-get update -qq > /dev/null && \
-        $SUDO apt-get install -qq python3 python3-six > /dev/null && \
+        $SUDO apt-get install -qq python3 python3-six apt-utils > /dev/null && \
         echo Installed!
 
     which yum > /dev/null && \
@@ -40,6 +41,19 @@ if ! which pip > /dev/null; then
     $SUDO ln -sf /usr/bin/pip3 /usr/bin/pip > /dev/null
 fi
 
+# Check if python3-venv is installed, if not, install it
+if ! dpkg -l | grep -q python3-venv; then
+    echo "Installing python3-venv..."
+    which apt-get > /dev/null && \
+        $SUDO apt-get update -qq > /dev/null && \
+        $SUDO apt-get install -qq python3-venv > /dev/null && \
+        echo "python3-venv installed!"
+
+    which yum > /dev/null && \
+        yum install -y python3-venv > /dev/null && \
+        echo "python3-venv installed!"
+fi
+
 # Install wget
 if ! which wget > /dev/null; then
     echo "Trying to install wget..."
@@ -56,11 +70,21 @@ fi
 
 # Download the orb
 cd /tmp || exit
+
 wget -q https://github.com/shipyardbuild/circleci-orb/archive/refs/heads/master.tar.gz
 tar xvzf master.tar.gz > /dev/null
 
 cd /tmp/circleci-orb-master/src/scripts || exit
 
-# Run the orb
+# Create a virtual environment
+python3 -m venv /tmp/orb_env
+
+# Activate the virtual environment
+# shellcheck disable=SC1091
+. /tmp/orb_env/bin/activate
+
+# Install the required packages
 pip install -r requirements.txt > /dev/null
-python orb.py 
+
+# Run the orb
+python orb.py
